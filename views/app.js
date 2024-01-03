777d0e55098fd68c9d8cb5b49d680855c4df670f
Emile0001 <boteseh@gmail.com>
2024-01-03 11:38:56 +0200

    fixed 500.ejs error

--- views/app.js
+++ views/app.js
@@ -3,15 +3,15 @@ const path = require("path");
 const express = require("express");
 const csurf = require("csurf");
 
-const db = require("./data/database");
-const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
-const errorHandlerMiddleware = require("./middlewares/error-handler");
-const authRoutes = require("./routes/auth.routes");
+const db = require("../data/database");
+const addCsrfTokenMiddleware = require("../middlewares/csrf-token");
+const errorHandlerMiddleware = require("../middlewares/error-handler");
+const authRoutes = require("../routes/auth.routes");
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname));
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
